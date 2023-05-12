fcc4570b8d458e6b0e75f1faffc210c2220d151c
Peter <peterahunanya79@gmail.com>
2023-05-12 17:53:54 +0400

    added-get-request

--- server.js
+++ server.js
@@ -8,3 +8,12 @@ const lessons = [
   { topic: 'biology', location: 'Brent Cross', price: 90 },
   { topic: 'web and mobile', location: 'India', price: 120 }
 ];
+
+app.get('/lessons', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.json(lessons);
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}/lessons`);
+  });
